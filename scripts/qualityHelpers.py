3ff0d31534919fe17963d4a8960dfa9adc75677a
Anne-Kathrin Baczko <abaczko@menja.oso.chalmers.se>
2025-08-28 14:06:37 +0200

    update filte

--- scripts/qualityHelpers.py
+++ scripts/qualityHelpers.py
@@ -13,6 +13,8 @@ from astropy.coordinates import *
 from datetime import timedelta, datetime
 from astropy.time import Time
 
+
+
 def calculate_expected_rms(ra,dec,obsdate,observation_time=8):
     # Define observer's latitude and longitude
     observer_lat = 52.9088  # latitude of LOFAR
@@ -41,13 +43,13 @@ def calculate_expected_rms(ra,dec,obsdate,observation_time=8):
     return correction_factor_rms
 
 ####
-def image_quality(rms, DR, peak, expected_rms, cut_DR=10, cut_rms=3):
+def image_quality(stats_matrix, expected_rms=75e-5, cut_peak=0.1, cut_rms=3):
     """
     Get image quality acceptance column
 
     Args:
-        csv_table: CSV with image-based scores
-        :cut_DR: Dynamic range of image
+        stats_matrix: dict with image-based scores
+        :cut_peak: Peak of image
         :cut_rms: RMS of image
     """
    # df = pd.read_csv(csv_table)
@@ -62,20 +64,23 @@ def image_quality(rms, DR, peak, expected_rms, cut_DR=10, cut_rms=3):
     #df.loc[mask, 'accept_image'] = True
    # df.to_csv(csv_table, index=False)
     #return csv_table
+
+    rms = stats_matrix["rms"]
+    peak = stats_matrix["peak"]
+    # Add this functions once we have useful values to input to calculate_expected_rms
+#    if not expected_rms:
+#        expected_rms = calculate_expected_rms(self.RA,self.DEC,self.Date,observation_time=8)
     rms_limit = expected_rms*cut_rms
-    DR_limit = cut_DR
+    peak_limit = cut_peak
     valid = False
-    valid = (rms <= rms_limit) and (DR >= DR_limit)
-    diagnostics = {
-            'rms': rms,
-            'dynamic_range': DR,
-            'peak': peak,
-            'rms_limit': rms_limit,
-            'dynamic_range_limit': DR_limit,
-            'valid': valid,
-            }
-    print(diagnostics)
-    return diagnostics
+    valid = (rms <= rms_limit) and (peak >= peak_limit)
+    stats_matrix ['rms_limit']= rms_limit
+    stats_matrix ['peak_limit']= peak_limit
+    stats_matrix ['valid']= valid
+
+    print("Final diagnostics of inage:\n")
+    print(stats_matrix)
+    return stats_matrix
 
 class ImageData(object):
     """ Load lofar image and get basic info
@@ -86,10 +91,12 @@ class ImageData(object):
                  catalogues=[],
                  noise_method="",
                  load_general_info="",
+                 facet = None,
                 ):
         self.fits_file = fits_file
         self.residual_file = residual_file
         self.rms = False
+        self.facet = False
         if fits_file!="":
             with fits.open(fits_file) as hdu:
                 self.hdu_list = hdu
@@ -100,6 +107,7 @@ class ImageData(object):
             except:
                 self.Z = self.image_data
 
+            self.id = fits_file.split('.')[0]
             self.pixelscale = abs(self.header["CDELT1"]) #in deg
             self.imagesize = self.header["NAXIS1"]
             self.RA = self.header["CRVAL1"]
@@ -118,36 +126,36 @@ class ImageData(object):
                     self.residual_Z = self.residual_data[0,0, :, :]
                 except:
                     self.residual_Z = self.residual_data
-    def get_rms(self,
-                maskSup:float = 1e-7,
-                sigma:int = 3.,
-                noise_method:str = "Histogram Fit",
-                use_residual_img = True,
-                plotfile = 'Histogram_fit.png',
-               ):
+
+    def get_rms(self,sigma=10.,noise_method= "Histogram Fit", use_residual_img = False, plotfile='Histogram_fit.png'):
         """
         Get rms from map
 
         Args:
-            :maskSup: mask theshold
+            :sigma: sigma for cutting the map
+            :noise_method: Noise method: "Histogram Fit"...
+            :use_residual_img: Set True if residual image should be used for histogram fit, False if total intensity image be used
+            :plot_file: plot name for saving the histogram plot
         """
         if self.residual_file !="":
             if use_residual_img:
                 print("Using residual image for estimating rms.\n")
-                self.rms = get_image_rms(self.residual_Z, maskSup=maskSup, noise_method=noise_method, residual_image=self.residual_Z,plotfile=plotfile)
+                self.rms = get_image_rms(self.residual_Z, noise_method=noise_method, residual_image=self.residual_Z,plotfile=plotfile,sigma=sigma)
             else:
                 print("Using image for estimating rms.\n")
-                self.rms = get_image_rms(self.Z, maskSup=maskSup, noise_method=noise_method, residual_image=self.residual_Z,plotfile=plotfile,clip=True)
+                self.rms = get_image_rms(self.Z, noise_method=noise_method, residual_image=self.residual_Z,plotfile=plotfile,clip=True,sigma=sigma)
         else:
             print("Using image for estimating rms.\n")
-            self.rms = get_image_rms(self.Z, maskSup=maskSup, noise_method=noise_method,plotfile=plotfile,clip=True)
+            self.rms = get_image_rms(self.Z, noise_method=noise_method,plotfile=plotfile,clip=True,sigma=sigma)
 
-        #self.expected_rms = calculate_expected_rms(self.RA,self.DEC,self.Date,observation_time=8)
-        self.expected_rms = 75e-5 #until we have a better expectation/functioning calculation
-
-        return self.rms,self.expected_rms  # jy/beam
+        return self.rms # jy/beam
 
     def get_statistics(self):
+        """
+        Get image statistics.
+        Returns:
+            :dict: with statistics
+        """
         self.peak = get_peakflux(self.image_data)
         self.min = get_min(self.image_data)
         self.minmax = get_minmax(self.min,self.peak)
@@ -156,13 +164,75 @@ class ImageData(object):
         else:
             self.rms = self.get_rms(self)
             self.dyn_range = get_dyn_range(self.peak,self.rms)
+        stats_matrix ={
+                'id': self.id,
+                'rms': self.rms,
+                'peak': self.peak,
+                'dyn_range': self.dyn_range,
+            }
 
-        print("Statistics derived:\nPeak={}\nMin={}\nMinMax={}\nDyn_range={}\n".format(self.peak,self.min,self.minmax,self.dyn_range))
-        return {"peak":self.peak,"min":self.min,"minmax":self.minmax,"DR":self.dyn_range, "RMS":self.rms}
+        self.stats_matrix = image_quality(stats_matrix)
 
-    def get_quality(self):
-        diagnostics = image_quality(self.rms, self.dyn_range, self.peak, self.expected_rms, cut_DR=10, cut_rms=3)
-        return diagnostics
+        self.stats_matrix = image_quality(self.stats_matrix)
+        return stats_matrix
+
+    def get_facet_statistics(self):
+        facet_ids = np.unique(self.facet)
+        self.facet_stats_matrix = []
+        for facet_id in facet_ids:
+            mask = (self.facet == facet_id)
+            facet_px = self.Z[mask]
+            if np.count_nonzero(mask) == 0:
+                continue #skip empty facets
+            rms = get_image_rms(facet_pixels,noise_method="Histogram Fit",clip=True)
+            peak = get_peakflux(facet_pixels)
+            dyn_range = get_dyn_range(peak,rms)
+
+            self.facet_stats_matrix.append({
+                'facet': facet_id,
+                'rms': rms,
+                'peak': peak,
+                'dyn_range': dyn_range,
+                'valid': validity,
+            })
+
+            self.facet_stats_matrix = image_quality(self.facet_stats_matrix)
+        return self.facet_stats_matrix
+#        if facet:
+#            #Load facet image
+#            self.facet = 
+#
+#
+#    def get_facet(self,facet_n=1):
+#        mask = (self.facet == facet_n)
+#        image_facet  = self.image_data[mask]
+#n_facets = 20
+#stats_matrix = []
+#
+#for facet_id in range(1, n_facets+1):
+#    mask = (facets_mask == facet_id)
+#    facet_pixels = Img[mask]
+#    if np.count_nonzero(mask) == 0:
+#        continue  # skip empty facets
+#
+#    rms = get_image_rms(facet_pixels, noise_method="Histogram Fit")
+#    peak = get_peakflux(facet_pixels)
+#    dyn_range = get_dyn_range(peak, rms)
+#    validity = dyn_range > DR_limit and rms < rms_limit  # or use your image_quality function
+#
+#    stats_matrix.append({
+#        'facet': facet_id,
+#        'rms': rms,
+#        'peak': peak,
+#        'dyn_range': dyn_range,
+#        'valid': validity,
+#    })
+
+# Convert to DataFrame for easy analysis
+#import pandas as pd
+#df_stats = pd.DataFrame(stats_matrix)
+#print(df_stats)
+
 
 ###########################
 
@@ -205,7 +275,7 @@ def get_dyn_range(peak,rms):
 ####
 def get_image_rms(image,
             maskSup:float = 1e-7,
-            sigma:int = 3.,
+            sigma:int = 10.,
             noise_method:str = "Histogram Fit",
             residual_image = False,
             plotfile = 'Histofram_fit.png',
@@ -221,12 +291,11 @@ def get_image_rms(image,
     if noise_method == "Histogram Fit":
         print("Deriving noise using a Histogram Fit.\n")
         Z1 = image.flatten()
-        sigma=10
         if clip:
-            print("Clipping data to < rms*sigma")
             m = Z1[np.abs(Z1) > maskSup]
             rmsold = np.std(m)
             med = np.median(m)
+            print("Clipping data to < rms*sigma= {}".format(rmsold,sigma))
             ind = np.where(np.abs(m-med) < rmsold*sigma)[0]
             Z1 = m[ind]
         Z1_2 = Z1 - np.min(Z1) + 10 ** (-5)
@@ -292,7 +361,6 @@ def get_image_rms(image,
         try:
             Z1 = residual_image.flatten()
             rms = np.nanstd(Z1)
-           # levs1 = sigma * noise
         except:
             raise Exception("If using Residual-method for rms, please provide the residual_image arg.")
     else:
@@ -301,7 +369,6 @@ def get_image_rms(image,
         if (noise_method=="Histogram Fit" and rms<=0):
             sys.stdout.write("Could not do Histogram Fit for noise, will use 'box' method\n")
         rms = 1.8*np.std(image[0:round(len(image)/10),0:round(len(image[0])/10)]) #factor 1.8 from self-cal errors
-        #levs1 = rms*sigma
     print("RMS calculated using method {} with value {}\n".format(noise_method,rms))
     return rms
 
